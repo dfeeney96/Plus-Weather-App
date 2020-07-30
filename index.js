6911196d4f86a4f3841f2095012e07bf0dce9b2f
dfeeney96 <daniellefeeney96@gmail.com>
2020-07-30 11:20:30 +0100

    Live date and time added

---
+++ index.js
@@ -0,0 +1,34 @@
+let now = new Date();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let date = now.getDate();
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let displayDateAndTime = document.querySelector("#display-date-and-time");
+displayDateAndTime.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
