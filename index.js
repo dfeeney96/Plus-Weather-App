07e974483d5649f9041eed27404bb960dd36e813
dfeeney96 <daniellefeeney96@gmail.com>
2020-07-30 12:04:37 +0100

    Current geolocation date, time, city name, temp added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// date and time
+
 let now = new Date();
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
@@ -32,3 +34,29 @@ if (minutes < 10) {
 
 let displayDateAndTime = document.querySelector("#display-date-and-time");
 displayDateAndTime.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
+
+//realtime data
+
+navigator.geolocation.getCurrentPosition(getPosition);
+
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  apiKey = `8ae2e00cca1079e06d3703b612c640a1`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  function defaultCurrentPosition(response) {
+    console.log(response);
+    let currentCityName = response.data.name;
+    let displayCityName = document.querySelector("#display-city-name");
+    displayCityName.innerHTML = currentCityName;
+
+    let currentCityTemp = Math.round(response.data.main.temp);
+
+    let displayCityTemp = document.querySelector("#display-city-temp");
+    displayCityTemp.innerHTML = `${currentCityTemp}°C`;
+  }
+
+  axios.get(apiUrl).then(defaultCurrentPosition);
+}
